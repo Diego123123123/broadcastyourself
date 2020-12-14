ae57b47da1af9c530a5305c83046b43b5ffdbf39
Diego <Diego.Mejia@fundacion-jala.org>
2020-12-14 02:33:00 -0400

    connect to firestore

--- app/src/main/java/cba/ucb/com/broadcastyoursselft/Profile.kt
+++ app/src/main/java/cba/ucb/com/broadcastyoursselft/Profile.kt
@@ -7,7 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 
-class Profile : AppCompatActivity() {
+class Profile : Posts() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
